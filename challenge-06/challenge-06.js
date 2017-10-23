0707e677415681825788d9e6d8f041e02b349638
Breno Oliveira <brenoo194@gmail.com>
2017-10-23 14:07:15 -0200

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Campeonato Mineiro';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ['Cruzeiro', 'atletico', 'Corinthias','palmeiras','vasco'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,19 +33,36 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+if (numero >= 1 && numero <= 5 ){
+	return 'O time que está em '+ numero +' lugar é o '+ teams[numero-1] +'.';
+}
+return 'Não temos a informação do time que esta nessa posicao';
+
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+showTeamPosition (1)
+"O time que está em 1 lugar é o Cruzeiro."
+showTeamPosition (2)
+"O time que está em 2 lugar é o atletico."
+showTeamPosition (3)
+"O time que está em 3 lugar é o Corinthias."
+showTeamPosition (4)
+"O time que está em 4 lugar é o palmeiras."
+showTeamPosition (6)
+"Não temos a informação do time que esta nessa posicao"
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+while (cont<=30){
+	console.log (cont++);
+
+} 
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
