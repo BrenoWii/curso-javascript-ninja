59f04048c4512fcc7524dca4a0994b06a24723ba
Breno Oliveira <brenoo194@gmail.com>
2017-10-24 15:50:00 -0200

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -43,14 +43,37 @@ VM226:1 O segundo array tem 2itens
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
-console.log( 'Números pares entre 10 e 20:' );
-// ?
+function teste (){
+	var cont=10;
+	
+	while (cont<=20){
+		result= cont %2;
+		if(result === 0 ){
+			console.log(cont);
+			
+        }
+	cont++;
+    }
+
+}
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log( 'Números ímpares entre 10 e 20:' );
-// ?
+function teste (){
+	var cont=10;
+	
+	while (cont<=20){
+		result= cont %2;
+		if(result === 1 ){
+			console.log(cont);
+			
+        }
+	cont++;
+    }
+
+}
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
